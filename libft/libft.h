809136ef2a262b0b817ea91be585c3dd75f2f8ea
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-04-17 12:14:33 +0100

    make libft.h the same as libft branch

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:17:10 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/04/16 19:19:16 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/04/17 12:14:05 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 void				ft_lstadd_front(t_list **lst, t_list *new);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-int					ft_lstclear(t_list **lst, void (*del)(void *));
 #endif
