a5f3736873457083b5b581278263fa41211ce903
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-04-18 18:23:00 +0100

    add functions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:17:10 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/04/16 19:19:16 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/04/18 18:22:18 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
 
 typedef struct s_list
 {
