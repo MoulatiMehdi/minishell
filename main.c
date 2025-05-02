b4953d40eaef1df39418e700783ff1100c944991
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-02 11:04:01 +0100

    add test

---
+++ main.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 17:09:35 by mmoulati          #+#    #+#             */
+/*   Updated: 2025/04/22 11:20:45 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "tokenizer.h"
+#include <readline/readline.h>
+#include <unistd.h>
+# include "expand.h"
+
+
+char	*ft_word_gettype(t_word *token)
+{
+    switch (token->type)
+    {
+        case WORD_QUOTE_DOUBLE:
+            return ("WORD_QUOTE_DOUBLE");
+        case WORD_QUOTE_SINGLE:
+            return ("WORD_QUOTE_SINGLE");
+        default:
+            return "WORD_NONE";
+    }
+}
+
+int	main(void)
+{
+    t_token	*token;
+    t_word * head;
+    char	*str;
+    char	*lexeme;
+
+    while (1)
+    {
+        token = NULL;
+        str = readline(" \033[32mMinishell\033[0m\033[31m>\033[0m ");
+        if (str == NULL)
+            break ;
+        token = tokenize(str);
+        lexer(token);
+        head = ft_expand_split(token);
+        while (head)
+        {
+            lexeme = NULL;
+            if(head->value != NULL)
+                lexeme = strndup(head->value, head->length);
+            printf("%-30s - %s\n",ft_word_gettype(head),lexeme);
+            free(lexeme);
+            head = head->next;
+        }
+        free(str);
+        ft_clear();
+    }
+    return (0);
+}
