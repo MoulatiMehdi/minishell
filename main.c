e8035a3b7e3a7f3abb196a5708e2b21446763286
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-06 16:25:37 +0000

    update test for excution

--- main.c
+++ main.c
@@ -6,15 +6,48 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:09:35 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/04/22 11:20:45 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:24:28 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
+#include "parser.h"
 
-char * ft_asttype_getstr(t_token_type type);
-t_ast	*ft_ast_andor(t_token **token);
+char ** ft_ast_getargs(t_ast * ast);
 
+void ft_ast_print_args(t_ast * ast)
+{
+    t_list * p;
+    t_ast * node;
+    char ** strs;
+    size_t i;
+   
+    if(ast == NULL)
+        return ;
+    if(ast->type == AST_SIMPLE_COMMAND)
+    {
+        strs = ft_ast_getargs(ast);
+        printf("\n*************************\n");
+        if(strs == NULL)
+        {
+            printf("NULL\n");
+            return ;
+        }
+        i = 0;
+        while(strs[i])
+        {
+            printf("%s\n",strs[i]);
+            i++;
+        }
+    }
+    p = ast->children;
+    while(p)
+    {
+        node = p->content;
+        ft_ast_print_args(node);
+        p = p->next;
+    }
+}
 int	main(void)
 {
     t_token	*token;
@@ -32,14 +65,16 @@ int	main(void)
         /*printf("LEXER : \n");*/
         /*ft_lexer_print(token);*/
         node = parser(token);
-        printf("\n*******************************************\n");
-        printf("\t ABSTRACT SYNTAX TREE \n");
-        printf("\t========================\n");
-        ft_ast_print(node,0);
-        printf("\n*******************************************\n");
-        printf("\t  COMMAND FROM AST \n");
-        printf("\t====================\n");
-        ft_ast_tocommand(node);
+        // printf("\n*******************************************\n");
+        // printf("\t ABSTRACT SYNTAX TREE \n");
+        // printf("\t========================\n");
+        // ft_ast_print(node,0);
+        // printf("\n*******************************************\n");
+        // printf("\t  COMMAND FROM AST \n");
+        // printf("\t====================\n");
+        // ft_ast_tocommand(node);
+   
+        ft_ast_print_args(node);
         printf("\n");
         ft_ast_free(node);
         free(str);
