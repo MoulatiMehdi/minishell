6e17b3cc69bc3abd7c069ed7369e1fbe63692fbf
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-06 18:21:29 +0000

    update main tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: okhourss <okhourss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:11:48 by okhourss          #+#    #+#             */
-/*   Updated: 2025/05/04 21:10:37 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/05/06 18:22:03 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int	main(void)
 	t_token	*token;
 	t_token	*token_cpy;
 	t_ast	*node;
-	char	*str;
+	t_list *p_list;
+    char	*str;
 
 	setvbuf(stdout, NULL, _IONBF, 0);
 	while (1)
@@ -46,10 +47,23 @@ int	main(void)
             if(token->type == TOKEN_WORD || (ft_token_isredirect(token->type) && token->value != NULL ))
             {
                 printf("\n**************************\n");
-                write(1,"\t\t ",3);
+                write(1,"\t\t token :  ",3);
                 write(1,token->value,token->length);
                 write(1," : \n",4);
                 ft_token_expand(token);
+                printf("**************************\n");
+                printf("FIELDS : \n");
+                if(token->fields)
+                {
+                    p_list = token->fields->head;
+                    while (p_list)
+                    {
+                        printf("- %s\n",(char *)p_list->content);
+                        p_list = p_list->next;
+                    }
+                }else {
+                    printf("(NULL)\n");
+                }
             }
             token = token ->next;
         }
