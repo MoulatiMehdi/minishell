942b9f2412af1643528c19492124ca10f054642b
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-10 21:35:36 +0000

    update execution test

--- main.c
+++ main.c
@@ -6,14 +6,16 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:09:35 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/05/07 22:23:43 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/05/10 18:28:30 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "config.h"
 #include "debug.h"
-#include "parser.h"
-# include "expansion.h"
 # include "execution.h"
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
 t_ast	*ft_ast_simplecommand(t_token **token);
@@ -34,15 +36,16 @@ void debug(t_ast * node)
 int	main(void)
 {
     t_token	*token;
-    t_token * p;
     t_ast * node;
-    t_list *p_lst;
+    pid_t pid;
     char	*str;
+    int wstatus;
+    unsigned char exit_code;
     
-    setvbuf(stdout, NULL, _IONBF, 0);
     while (1)
     {
-        str = readline(" \033[32mMinishell\033[0m\033[31m>\033[0m ");
+        ft_putstr_fd(SHELL_PROMPT,2);
+        str = readline("");
         if (str == NULL)
             break ;
         token = tokenize(str);
@@ -50,9 +53,20 @@ int	main(void)
         /*printf("LEXER : \n");*/
         /*ft_lexer_print(token);*/
         node = ft_ast_simplecommand(&token);
-
         ft_execute_simplecommand(node);
-        wait4(-1,NULL,0,NULL);
+        waitpid(-1,&wstatus,0);
+        if(WIFEXITED(wstatus))
+            exit_code = WEXITSTATUS(wstatus);
+        if(WIFSIGNALED(wstatus))
+            exit_code = WTERMSIG(wstatus);
+        if(WIFSTOPPED(wstatus) )
+            exit_code = WSTOPSIG(wstatus);
+        if(WIFCONTINUED(wstatus))
+        {
+
+            printf("process resumed\n");
+        }
+        printf("Status : %d\n",exit_code);
         ft_ast_free(node);
         free(str);
     }
