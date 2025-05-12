588a5d7fa0f92b3d373e1141fbaa914be5c47068
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-12 23:44:16 +0000

    update main file

--- main.c
+++ main.c
@@ -6,19 +6,12 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:09:35 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/05/10 18:28:30 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/05/12 23:39:06 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "config.h"
 #include "debug.h"
 # include "execution.h"
-#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-
-t_ast	*ft_ast_simplecommand(t_token **token);
 
 void debug(t_ast * node)
 {
@@ -33,42 +26,16 @@ void debug(t_ast * node)
         printf("\n");
 }
 
+bool ft_shell_isinteractive()
+{
+    return isatty(STDIN_FILENO) && isatty(STDERR_FILENO);
+}
+
 int	main(void)
 {
-    t_token	*token;
-    t_ast * node;
-    pid_t pid;
-    char	*str;
-    int wstatus;
-    unsigned char exit_code;
-    
-    while (1)
-    {
-        ft_putstr_fd(SHELL_PROMPT,2);
-        str = readline("");
-        if (str == NULL)
-            break ;
-        token = tokenize(str);
-        lexer(token);
-        /*printf("LEXER : \n");*/
-        /*ft_lexer_print(token);*/
-        node = ft_ast_simplecommand(&token);
-        ft_execute_simplecommand(node);
-        waitpid(-1,&wstatus,0);
-        if(WIFEXITED(wstatus))
-            exit_code = WEXITSTATUS(wstatus);
-        if(WIFSIGNALED(wstatus))
-            exit_code = WTERMSIG(wstatus);
-        if(WIFSTOPPED(wstatus) )
-            exit_code = WSTOPSIG(wstatus);
-        if(WIFCONTINUED(wstatus))
-        {
-
-            printf("process resumed\n");
-        }
-        printf("Status : %d\n",exit_code);
-        ft_ast_free(node);
-        free(str);
-    }
+    if(ft_shell_isinteractive())
+        return ft_shell_interactive();
+    else 
+        return ft_shell_noninteractive();
     return (0);
 }
