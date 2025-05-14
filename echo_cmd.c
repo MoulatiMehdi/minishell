e72de21948df0f7f1abad979fe2b97de1a9c4fa2
Oussama Khourssa <okhourss@c3r1p3.1337.ma>
2025-05-14 15:07:42 +0100

    improving echo cmd

--- echo_cmd.c
+++ echo_cmd.c
@@ -6,43 +6,47 @@
 /*   By: okhourss <okhourss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:06:52 by okhourss          #+#    #+#             */
-/*   Updated: 2025/05/12 11:32:21 by okhourss         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:04:01 by okhourss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+static int is_all_ns(const char *s)
+{
+    int i;
+
+    if (!s || s[0] != '-' || s[1] != 'n')
+        return (0);
+    for (i = 2; s[i]; i++)
+        if (s[i] != 'n')
+            return (0);
+    return (1);
+}
+
+
 int echo_cmd(t_cmd *cmd)
 {
-	int i = 0;
-	int j = 0;
-	int new_line = 1;
-	if (!cmd)
-		return 1;
-	//! need to be removed later
-	if (strcmp(cmd->name, "echo") != 0)
-		return 127;
+    int arg_idx = 0;
+    int print_idx;
+    int omit_nl  = 1;
 
-	while (cmd->args && cmd->args[j] && ft_strncmp(cmd->args[j], "-n", 2) == 0)
-	{
-		int k = 2;
-		while (cmd->args[j][k] == 'n')
-			k++;
-		if (cmd->args[j][k] != '\0')
-			break;
-		new_line = 0;
-		j++;
-	}
-	cmd->args = &cmd->args[j];
-
-	while (cmd->args && cmd->args[i])
-	{
-		ft_putstr_fd(cmd->args[i], 1);
-		if (cmd->args[i + 1])
-			write(1, " ", 1);
-		i++;
-	}
-	if (new_line)
-		write(1, "\n", 1);
-	return 0;
+    if (!cmd)
+        return (1);
+    while (cmd->args[arg_idx] && is_all_ns(cmd->args[arg_idx]))
+    {
+        omit_nl = 0;
+        arg_idx++;
+    }
+	print_idx = arg_idx; 
+    while (cmd->args[print_idx])
+    {
+        ft_putstr_fd(cmd->args[print_idx], 1);
+        if (cmd->args[print_idx + 1])
+            write(1, " ", 1);
+		print_idx++;
+    }
+    if (omit_nl)
+        write(1, "\n", 1);
+    return (0);
 }
