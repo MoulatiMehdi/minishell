d957ecb67ba696d828a6cfbd9ad063ef041ef9ee
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-15 12:09:36 +0100

    update main.c function

--- main.c
+++ main.c
@@ -17,60 +17,60 @@
 
 char	*ft_token_type(t_token *token)
 {
-    switch (token->type)
-    {
-        case TOKEN_EOI:
-            return ("TOKEN_EOI");
-        case TOKEN_WORD:
-            return ("TOKEN_WORD");
-        case TOKEN_PIPE:
-            return ("TOKEN_PIPE");
-        case TOKEN_AND:
-            return ("TOKEN_AND");
-        case TOKEN_OR:
-            return ("TOKEN_OR");
-        case TOKEN_REDIRECT_APPEND:
-            return ("TOKEN_REDIRECT_APPEND");
-        case TOKEN_REDIRECT_IN:
-            return ("TOKEN_REDIRECT_IN");
-        case TOKEN_REDIRECT_OUT:
-            return ("TOKEN_REDIRECT_OUT");
-        case TOKEN_REDIRECT_HERE:
-            return ("TOKEN_REDIRECT_HERE");
-        case TOKEN_PARENS_OPEN:
-            return ("TOKEN_PARENS_OPEN");
-        case TOKEN_PARENS_CLOSE:
-            return ("TOKEN_PARENS_CLOSE");
-        default:
-            return ("TOKEN_UNKNOWN");
-    }
+	switch (token->type)
+	{
+	case TOKEN_EOI:
+		return ("TOKEN_EOI");
+	case TOKEN_WORD:
+		return ("TOKEN_WORD");
+	case TOKEN_PIPE:
+		return ("TOKEN_PIPE");
+	case TOKEN_AND:
+		return ("TOKEN_AND");
+	case TOKEN_OR:
+		return ("TOKEN_OR");
+	case TOKEN_REDIRECT_APPEND:
+		return ("TOKEN_REDIRECT_APPEND");
+	case TOKEN_REDIRECT_IN:
+		return ("TOKEN_REDIRECT_IN");
+	case TOKEN_REDIRECT_OUT:
+		return ("TOKEN_REDIRECT_OUT");
+	case TOKEN_REDIRECT_HERE:
+		return ("TOKEN_REDIRECT_HERE");
+	case TOKEN_PARENS_OPEN:
+		return ("TOKEN_PARENS_OPEN");
+	case TOKEN_PARENS_CLOSE:
+		return ("TOKEN_PARENS_CLOSE");
+	default:
+		return ("TOKEN_UNKNOWN");
+	}
 }
 
 int	main(void)
 {
-    t_token	*token;
-    char	*str;
-    char	*lexeme;
+	t_token	*token;
+	char	*str;
+	char	*lexeme;
 
-    while (1)
-    {
-        token = NULL;
-        str = readline(" \033[32mMinishell\033[0m\033[31m>\033[0m ");
-        if (str == NULL)
-            break ;
-        token = tokenize(str);
-        lexer(token);
-        while (token)
-        {
-            lexeme = NULL;
-            if(token->value != NULL)
-                lexeme = strndup(token->value, token->length);
-            printf("%-30s : %s\n", ft_token_type(token), lexeme);
-            free(lexeme);
-            token = token->next;
-        }
-        free(str);
-        ft_clear();
-    }
-    return (0);
+	while (1)
+	{
+		token = NULL;
+		str = readline(" \033[32mMinishell\033[0m\033[31m>\033[0m ");
+		if (str == NULL)
+			break ;
+		token = tokenize(str);
+		lexer(token);
+		while (token)
+		{
+			lexeme = NULL;
+			if (token->value != NULL)
+				lexeme = strndup(token->value, token->length);
+			printf("%-30s : %s\n", ft_token_type(token), lexeme);
+			free(lexeme);
+			token = token->next;
+		}
+		free(str);
+		ft_clear();
+	}
+	return (0);
 }
