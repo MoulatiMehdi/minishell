985a7e0dc6f2a81565348b1db1a1f9e24fca0a9d
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-18 17:10:09 +0100

    update header

--- parser.h
+++ parser.h
@@ -51,5 +51,6 @@ volatile sig_atomic_t	*ft_sigint_recieved(void);
 void					ft_signal_int(int signal);
 void					ft_signal_quit(int signal);
 void					ft_signal_bashignore(void);
+void					ft_heredoc_sigint(int signal);
 t_list					*ft_list_token_push(t_list **head, t_token *token);
 #endif // !PARSER_H
