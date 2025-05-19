1352a2dae6bf04b3b964921ea6bbd9f4cfde934a
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-19 15:23:43 +0100

    update built-ins test

---
+++ execution.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 18:37:22 by mmoulati          #+#    #+#             */
+/*   Updated: 2025/05/16 18:37:23 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTION_H
+# define EXECUTION_H
+
+# include "libft.h"
+# include "libft/libft.h"
+# include "parser.h"
+# include "tokenizer.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+
+void			ft_signal_bashignore(void);
+void			ft_signal_init(void);
+
+int				ft_execute_simplecommand(t_ast *ast);
+int				ft_execute_buildin(t_list *redirect, char **args);
+int				ft_execute_file(t_list *redirect, char **args);
+
+char			**ft_ast_getargs(t_ast *ast);
+int				ft_command_isbuildin(char *str);
+
+bool			ft_path_isdir(char *pathname);
+bool			ft_path_isfile(char *pathname);
+char			*ft_path_join(char *p1, char *p2);
+char			**ft_path_get(void);
+
+void			ft_perror(char *name, char *msg);
+void			ft_token_error(t_token *token, char *msg);
+
+unsigned char	ft_shell_interactive(void);
+unsigned char	ft_shell_noninteractive(void);
+void			ft_signal_int(int signal);
+int				ft_redirect(t_list *redirect);
+void			ft_heredoc_sigint(int signal);
+void			ft_execute_exit(int status);
+#endif
--- main.c
+++ main.c
@@ -10,21 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "builtins.h"
 #include "debug.h"
 #include "libft/libft.h"
 #include "parser.h"
+#include <readline/readline.h>
+#include <string.h>
 
-char * ft_asttype_getstr(t_token_type type);
-t_ast	*ft_ast_andor(t_token **token);
+t_ast	*ft_ast_simplecommand(t_token **token);
+char	**ft_ast_getargs(t_ast *ast);
 
-int	main(void)
+
+t_array * ft_env_create(char ** strs)
 {
+    size_t i;
+    char * tmp;
+    t_array * array;
+
+    array= NULL;
+    i = 0;
+    while(strs[i])
+    {
+        tmp = ft_strdup(strs[i]);
+        ft_collector_track(tmp);
+        ft_array_push(&array,tmp);
+        i++;
+    }
+    return array;
+}
+
+void ft_field_push(t_token * token)
+{
+
+    char * str;
+
+    while(token)
+    {
+        str = ft_strndup(token->value,token->length);
+        ft_collector_track(str);
+        ft_array_push(&token->fields,str);
+        token = token->next;
+    }
+}
+
+int	main(int argc,char **argv,char **envp)
+{
+
     t_token	*token;
     t_ast * node;
     char	*str;
-
+    char ** strs;
+    t_cmd cmd;
+   
+    (void) argv;
+    (void) argc;
     ft_signal_bashignore();
     setvbuf(stdout, NULL, _IONBF, 0);
+    rl_outstream = stderr;
     while (1)
     {
         str = readline(" \033[32mMinishell\033[0m\033[31m>\033[0m ");
@@ -32,20 +74,31 @@ int	main(void)
             break ;
         token = tokenize(str);
         lexer(token);
-        /*printf("LEXER : \n");*/
-        /*ft_lexer_print(token);*/
-        node = parser(token);
-        printf("\n*******************************************\n");
-        printf("\t ABSTRACT SYNTAX TREE \n");
-        printf("\t========================\n");
-        ft_ast_print(node,0);
-        printf("\n*******************************************\n");
-        printf("\t  COMMAND FROM AST \n");
-        printf("\t====================\n");
-        ft_ast_tocommand(node);
-        printf("\n");
-        free(str);
-        ft_clear();
+        ft_field_push(token);
+        node = ft_ast_simplecommand(&token);
+        strs = ft_ast_getargs(node);
+        if(strs != NULL)
+        {
+            cmd.name = strs[0];
+            cmd.args = &strs[1];
+            cmd.env = ft_env_create(envp);
+           
+            if(ft_strcmp(cmd.name,"echo") == 0)
+                echo_cmd(&cmd);
+            if(ft_strcmp(cmd.name,"cd") == 0)
+                cd_cmd(&cmd);
+            if(ft_strcmp(cmd.name,"unset") == 0)
+                unset_cmd(&cmd);
+            if(ft_strcmp(cmd.name,"exit") == 0)
+                exit_cmd(&cmd,0);
+            if(ft_strcmp(cmd.name,"pwd") == 0)
+                pwd_cmd(&cmd);
+            if(ft_strcmp(cmd.name,"env") == 0)
+                env_cmd(&cmd);
+            if(ft_strcmp(cmd.name,"export") == 0)
+                export_cmd(&cmd);
+        }
+        /*ft_clear();*/
     }
     return (0);
 }
