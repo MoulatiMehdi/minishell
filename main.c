6bbe2d8cbb7114e46013ed1a604a73074b21c195
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-24 15:32:05 +0100

    update change on the main file

--- main.c
+++ main.c
@@ -9,56 +9,64 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "debug.h"
+#include "debug.h"
+#include "expansion.h"
 #include "libft/libft.h"
 #include "parser.h"
 #include "tokenizer.h"
-#include "expansion.h"
-#include <stdio.h>
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
 
-int main(void)
+t_list		*ft_pattern_files(char *word, char *mask);
+
+static void	print(void *str)
 {
-	t_token *token;
-	t_ast *node;
-	char *str;
+	printf("%s\n", (char *)str);
+}
+int	main(int argc, char **argv)
+{
+	t_list	*head;
 
+	/*t_token	*token;*/
+	/*t_ast	*node;*/
+	/*char	*str;*/
+	if (argc != 3)
+		return (2);
 	setvbuf(stdout, NULL, _IONBF, 0);
-	while (1)
-	{
-		str = readline("\033[32mMinishell\033[0m\033[31m>\033[0m ");
-		if (str == NULL)
-			break;
-        if(*str)
-            add_history(str);
-		token = tokenize(str);
-		if (!token)
-		{
-			free(str);
-			continue;
-		}
-		lexer(token);
-		node = parser(token);
-
-		if (!node)
-		{
-			printf("Parse error.\n");
-			free(str);
-			continue;
-		}
-    
-        while(token)
-        {
-            expand_token(token);
-            token = token->next;
-        }
-		ft_ast_print(node,0);
-		free(str);
-	    ft_clear();
-    }
-    clear_history();
+	head = ft_pattern_files(argv[1], argv[2]);
+	ft_lstiter(head, print);
+	/*while (1)*/
+	/*{*/
+	/*	str = readline("\033[32mMinishell\033[0m\033[31m>\033[0m ");*/
+	/*	if (str == NULL)*/
+	/*		break ;*/
+	/*	if (*str)*/
+	/*		add_history(str);*/
+	/*	token = tokenize(str);*/
+	/*	if (!token)*/
+	/*	{*/
+	/*		free(str);*/
+	/*		continue ;*/
+	/*	}*/
+	/*	lexer(token);*/
+	/*	node = parser(token);*/
+	/*	if (!node)*/
+	/*	{*/
+	/*		printf("Parse error.\n");*/
+	/*		free(str);*/
+	/*		continue ;*/
+	/*	}*/
+	/*	while (token)*/
+	/*	{*/
+	/*		expand_token(token);*/
+	/*		token = token->next;*/
+	/*	}*/
+	/*	ft_ast_print(node, 0);*/
+	/*	free(str);*/
+	/*	ft_clear();*/
+	/*}*/
+	/*clear_history();*/
+	/*return (0);*/
 	return (0);
 }
-
-
