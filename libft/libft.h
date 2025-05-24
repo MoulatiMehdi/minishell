edafd17ddc7212a11203295de84f6981aa512135
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-24 15:08:46 +0100

    add list_strconcat function

--- libft/libft.h
+++ libft/libft.h
@@ -77,6 +77,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
+void				ft_list_strconcat(t_list *list, char *str);
 
 void				ft_sorted_list_merge(t_list **head1, t_list *head2,
 						int (*cmp)(void *s1, void *s2));
