7bbef012946fd1a7c32d9b2b725da639e520393d
Oussama Khourssa <okhourss@c3r3p3.1337.ma>
2025-05-26 18:54:33 +0100

    update unset code

--- unset_cmd.c
+++ unset_cmd.c
@@ -6,11 +6,27 @@
 /*   By: okhourss <okhourss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:39:54 by okhourss          #+#    #+#             */
-/*   Updated: 2025/05/26 17:23:59 by okhourss         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:53:28 by okhourss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+
+int	ft_strcmp_key(const char *key, const char *content)
+{
+	size_t	i;
+
+	i = 0;
+	while (key[i] && content[i] && content[i] != '=')
+	{
+		if (key[i] != content[i])
+			return (key[i] - content[i]);
+		i++;
+	}
+	if (key[i] == '\0' && content[i] == '=')
+		return (0);
+	return (key[i] - content[i]);
+}
 
 int	unset_cmd(char **args)
 {
@@ -25,7 +41,7 @@ int	unset_cmd(char **args)
 	i = 0;
 	while (args[i])
 	{
-		ft_array_remove(&env, args[i], ft_strcmp, free);
+		ft_array_remove(&env, args[i], ft_strcmp_key, free);
 		i++;
 	}
 	return (0);
