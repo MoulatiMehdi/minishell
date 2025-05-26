cab8994220a527546346aacf7f43fecdf99f4ace
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-26 15:44:27 +0100

    remove debugging functions and headers

--- main.c
+++ main.c
@@ -6,26 +6,12 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:09:35 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/05/12 23:39:06 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/05/26 15:43:08 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "debug.h"
 #include "execution.h"
 #include "status.h"
-
-void	debug(t_ast *node)
-{
-	printf("\n*******************************************\n");
-	printf("\t ABSTRACT SYNTAX TREE \n");
-	printf("\t========================\n");
-	ft_ast_print(node, 0);
-	printf("\n*******************************************\n");
-	printf("\t  COMMAND FROM AST \n");
-	printf("\t====================\n");
-	ft_ast_tocommand(node);
-	printf("\n");
-}
 
 int	main(void)
 {
