6b59659d4d2ff15a91925de59c1df662b9bb95f3
Oussama Khourssa <okhourss@c3r3p3.1337.ma>
2025-05-26 17:24:59 +0100

    update unset cmd

--- unset_cmd.c
+++ unset_cmd.c
@@ -6,37 +6,11 @@
 /*   By: okhourss <okhourss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:39:54 by okhourss          #+#    #+#             */
-/*   Updated: 2025/05/26 11:38:00 by okhourss         ###   ########.fr       */
+/*   Updated: 2025/05/26 17:23:59 by okhourss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-
-void	ft_unset_var(char *var, t_array *env)
-{
-	t_list	*curr;
-	t_list	*prev;
-
-	if (!var || !env || !env->head)
-		return ;
-	curr = ft_get_env(var, env);
-	if (!curr)
-		return ;
-	prev = env->head;
-	if (prev == curr)
-	{
-		env->head = curr->next;
-	}
-	else
-	{
-		while (prev->next && prev->next != curr)
-			prev = prev->next;
-		if (prev->next == curr)
-			prev->next = curr->next;
-	}
-	free(curr->content);
-	free(curr);
-}
 
 int	unset_cmd(char **args)
 {
@@ -51,7 +25,7 @@ int	unset_cmd(char **args)
 	i = 0;
 	while (args[i])
 	{
-		ft_unset_var(args[i], env);
+		ft_array_remove(&env, args[i], ft_strcmp, free);
 		i++;
 	}
 	return (0);
