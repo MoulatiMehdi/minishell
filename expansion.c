e8de87462abf05d4ebe394f535fc2f5a398bb2b5
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-27 14:50:29 +0100

    remove debugging functions from files

--- expansion.c
+++ expansion.c
@@ -16,27 +16,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char * ft_word_type(t_word * word)
+char	*ft_word_type(t_word *word)
 {
-    if(word->type == WORD_QUOTE_SINGLE)
-        return "WORD_QUOTE_SINGLE";
-    else  if(word->type == WORD_QUOTE_DOUBLE)
-            return "WORD_QUOTE_DOUBLE";
-    else  if(word->type == WORD_NONE)
-            return "WORD_NONE";
-    else  if(word->type == WORD_WILDCARD)
-            return "WORD_WILDCARD";
-    else 
-        return "WORD_UNKNOWN";
-
-}
-void ft_word_print(t_word * tmp)
-{
-    while (tmp)
-	{
-        printf("- %s %s\n",ft_word_type(tmp),ft_strndup_track(tmp->value,tmp->length));
-		tmp = tmp->next;
-	}
+	if (word->type == WORD_QUOTE_SINGLE)
+		return ("WORD_QUOTE_SINGLE");
+	else if (word->type == WORD_QUOTE_DOUBLE)
+		return ("WORD_QUOTE_DOUBLE");
+	else if (word->type == WORD_NONE)
+		return ("WORD_NONE");
+	else if (word->type == WORD_WILDCARD)
+		return ("WORD_WILDCARD");
+	else
+		return ("WORD_UNKNOWN");
 }
 
 void	expand_token(t_token *token)
@@ -44,10 +35,9 @@ void	expand_token(t_token *token)
 	t_word	*words;
 	t_array	*fields;
 	char	*str;
-    t_word * tmp;
+	t_word	*tmp;
+
 	words = ft_word_split(token);
-    printf("splitting : \n");
-    ft_word_print(words);
 	if (!words)
 		return ;
 	tmp = words;
@@ -56,14 +46,10 @@ void	expand_token(t_token *token)
 		expand_param(tmp);
 		tmp = tmp->next;
 	}
-    printf("expan params : \n");
-    ft_word_print(words);
 	if (token->type != TOKEN_REDIRECT_HERE)
 	{
 		join_quotes(words);
-        printf("quote join : \n");
-		ft_word_print(words);
-        fields = field_splitting(words);
+		fields = field_splitting(words);
 		ft_pathname_expansion(token, fields);
 		return ;
 	}
