ce1cc04b61bc5953660110b68426402534ddcd40
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-05-27 09:40:51 +0100

    add tracked array

--- libft/ft_array.c
+++ libft/ft_array.c
@@ -58,3 +58,42 @@ void	ft_array_destroy(t_array **array, t_func_free free_fct)
 	free(*array);
 	*array = NULL;
 }
+
+t_array	*ft_tracked_array_new(void)
+{
+	t_array	*array;
+
+	array = ft_malloc(sizeof(t_array));
+	if (array == NULL)
+		return (NULL);
+	array->head = NULL;
+	array->last = NULL;
+	array->length = 0;
+	return (array);
+}
+
+void	*ft_tracked_array_push(t_array **array, void *content)
+{
+	t_list	*node;
+
+	if (array == NULL || content == NULL)
+		return (NULL);
+	node = ft_lstnew(content);
+	ft_collector_track(node);
+	if (node == NULL)
+		return (NULL);
+	if (*array == NULL)
+		*array = ft_tracked_array_new();
+	if (*array == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	if ((*array)->head == NULL)
+		(*array)->head = node;
+	if ((*array)->last != NULL)
+		(*array)->last->next = node;
+	(*array)->last = ft_lstlast(node);
+	(*array)->length++;
+	return (content);
+}
