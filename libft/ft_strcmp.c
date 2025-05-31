39c43fe8a3545006a66ffd5986e52b0b44663d40
Oussama Khourssa <okhourss@c3r3p3.1337.ma>
2025-05-31 12:52:35 +0100

    fix ft_strcmp

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: okhourss <okhourss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 22:23:25 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/01/28 22:30:14 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/05/31 12:50:33 by okhourss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	unsigned int	i;
-
-	if (s1 == 0 ^ s2 == 0)
+	if (s1 == s2)
+		return (0);
+	if ((s1 == 0) ^ (s2 == 0))
 		return (1);
 	i = 0;
 	while (s1[i] == s2[i] && s1[i])
